30095c553f7ab56f2dd0478cb29ac0d0f61bb376
AhteshamUddowla <ahteshamiiuc@gmail.com>
2023-06-20 15:52:40 +0600

    todo

--- todo/main.js
+++ todo/main.js
@@ -1,4 +1,10 @@
-let todo_list = [];
+let todo_list = JSON.parse(localStorage.getItem('todo_list'));
+
+if(todo_list === null){
+    todo_list = [];
+}
+
+display();
 
 function display(){
     let html_list = "";
@@ -7,14 +13,26 @@ function display(){
         let html = `
             <div> ${todo_list[i].todo_value} </div>
             <div> ${todo_list[i].date_value} </div>
-            <button onclick="todo_list.splice(${i},1); display();" class="delete-btn">
+            <button 
+                class="delete-btn">
                 Delete 
             </button>
         `;
         html_list += html;
     }
     document.querySelector('.todo_list').innerHTML = html_list;
+    document.querySelectorAll('.delete-btn').forEach((deleteBtn, index) => {
+        deleteBtn.addEventListener('click', () => {
+            todo_list.splice(index,1);
+            localStorage.setItem('todo_list', JSON.stringify(todo_list));
+            display();
+        })
+    })
 }
+
+document.querySelector('.add-btn').addEventListener('click', () => {
+    add_todo();
+})
 
 function add_todo(){
     let todo_task = document.querySelector('.todo');
@@ -22,10 +40,12 @@ function add_todo(){
 
     let todo_value = todo_task.value;
     let date_value = date.value;
+    
+    if(todo_value!='')
+        todo_list.push({todo_value, date_value});
 
-    todo_list.push({todo_value, date_value});
+    localStorage.setItem('todo_list', JSON.stringify(todo_list));
 
-    // console.log(todo_list);
     todo_task.value = "";
     date.value = "";
     display();
